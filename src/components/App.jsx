a0fb3f00e31e7faa96ae77c0f60dc292a46c86f5
Maks Thimbarov <imaksim2@icloud.com>
2023-01-20 17:22:58 +0200

    finish job

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,12 +7,25 @@ class App extends Component {
   state = {
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: 'id-2', name: 'Roie Simpson', number: '459-12-56' },
+      { id: 'id-3', name: 'Rose Simpson', number: '459-12-56' },
+      { id: 'id-4', name: 'Rsie Simpson', number: '459-12-56' },
     ],
     filter: '',
   };
+
+  componentDidMount() {
+    JSON.parse(localStorage.getItem('contacts'))
+      ? this.setState({
+          contacts: JSON.parse(localStorage.getItem('contacts')),
+        })
+      : this.setState({ contacts: this.state.contacts });
+  }
+
+  componentDidUpdate() {
+    localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  }
+
   onSubmit = data => {
     const contact = { ...data };
     this.state.contacts.find(
